9b3480da0eb58bf1afbefa29e545a3c994517917
Xriss Xross <xriss_xross@outlook.com>
2023-08-17 11:04:26 +0100

    collisions working and attack cooldown

--- player.py
+++ player.py
@@ -6,22 +6,28 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('./assets/knight.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (48, 48))
         self.rect = self.image.get_rect(topleft = pos)
-        self.movement_direction = pygame.math.Vector2() #  creates a vector with x and y values
+
         self.obstruction = obstruction
+        self.movement_direction = pygame.math.Vector2() #  creates a vector with x and y values
+        self.attack_cooldown = 20
+        self.cooldown = 20
+
+
+    def asset_loader(self):
+        pass
+
 
     def input_listener(self):
         #  all keys are detected
         input = pygame.key.get_pressed()
         
-        #  in case the player pressed both directions at the same time
+        #  movement input
         if input[pygame.K_w] and input[pygame.K_s]:
             self.movement_direction.y = 0
-        #  if the w/s keys are being pressed manipulate the direction vector we created earlier
         elif input[pygame.K_w]:
             self.movement_direction.y = -1
         elif input[pygame.K_s]:
             self.movement_direction.y = 1
-        #  otherwise reset to 0 (other wise the player would move infinitely)
         else:
              self.movement_direction.y = 0
 
@@ -34,20 +40,34 @@ class Player(pygame.sprite.Sprite):
         else:
             self.movement_direction.x = 0
 
+        #  attack input
+        if input[pygame.K_SPACE] and self.attack_cooldown == self.cooldown:
+            print('attack')
+            self.attack_cooldown = 0
+
+
+    def cooldown_handler(self):
+        if self.attack_cooldown != self.cooldown:
+            self.attack_cooldown += 1
+        
+
     def move(self, speed):
         #  if you move diagonally you gain speed which although is something that is weird
-        #  i think its a pretty funny feature that will aid in speedrunning for players
-        self.rect.topleft += self.movement_direction * speed
+        #  I think its a pretty funny feature that will aid in speedrunning for players
+        self.rect.x += self.movement_direction.x * speed
         self.collide('x')
+        self.rect.y += self.movement_direction.y * speed
         self.collide('y')
+
 
     def collide(self, movement_direction):
         if movement_direction == 'x':
             for obstacle in self.obstruction:
+                #  check every single obstacle
                 if obstacle.rect.colliderect(self.rect):
-                    if self.movement_direction.x > 0:
+                    if self.movement_direction.x > 0: #  player must be moving right
                         self.rect.right = obstacle.rect.left
-                    if self.movement_direction.x < 0:
+                    if self.movement_direction.x < 0: #  player must be moving left
                         self.rect.left = obstacle.rect.right
                         
         if movement_direction == 'y':
@@ -58,6 +78,8 @@ class Player(pygame.sprite.Sprite):
                     if self.movement_direction.y < 0:
                         self.rect.top = obstacle.rect.bottom
 
+
     def update(self):
+        self.cooldown_handler()
         self.input_listener()
         self.move(3)
